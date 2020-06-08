35af63b143d904517c75f68cd27ea426cbf1cd48
Keith Dumont <keithdumont@Keiths-MacBook-Pro.local>
2020-06-08 11:14:01 -0700

    second commit

--- automation.py
+++ automation.py
@@ -2,6 +2,20 @@ from selenium import webdriver
 
 chrome_browser = webdriver.Chrome('./chromedriver')
 
-chrome_browser.maximize_window()
+chrome_browser.get(
+    'https://www.seleniumeasy.com/test/basic-first-form-demo.html')
 
-print(chrome_browser)
+
+show_message_button = chrome_browser.find_element_by_class_name('btn-default')
+
+user_message = chrome_browser.find_element_by_id('user-message')
+
+user_message.clear()
+
+user_message.send_keys('hi, how are you?')
+
+show_message_button.click()
+
+output_message = chrome_browser.find_element_by_id('display')
+
+chrome_browser.quit()
